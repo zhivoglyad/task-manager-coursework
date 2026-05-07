f230134a067f91b452ce690cf78ada80cc8d49dd
zhivoglyad <dariya.zhivoglyad@gmail.com>
2026-05-07 23:46:20 +0500

    feat(theme): add ThemeProvider syncing html class

---
+++ src/features/theme/ThemeProvider.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+import { useThemeStore } from './themeStore'
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const theme = useThemeStore((s) => s.theme)
+
+  useEffect(() => {
+    const root = document.documentElement
+    if (theme === 'dark') {
+      root.classList.add('dark')
+    } else {
+      root.classList.remove('dark')
+    }
+  }, [theme])
+
+  return <>{children}</>
+}
